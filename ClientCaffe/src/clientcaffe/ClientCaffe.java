d0a68d3a8d752472c0cd2c7ef010e40c88be3861
ZTDavide <59927455+ZTDavide@users.noreply.github.com>
2022-10-18 11:40:47 +0200

    Client finito da testare

--- ClientCaffe/src/clientcaffe/ClientCaffe.java
+++ ClientCaffe/src/clientcaffe/ClientCaffe.java
@@ -24,7 +24,7 @@ public class ClientCaffe {
     /**
      * @param connection
      */
-    public ClientCaffe() throws IOException{
+    public ClientCaffe() throws IOException {
 
         this.connection = new Socket("localhost", 12345);
         input = this.connection.getInputStream();
@@ -32,97 +32,110 @@ public class ClientCaffe {
 
     }
 
-    public void main(String[] args) {
-       
-        
+    public static void main(String[] args) {
+
         try {
             // TODO code application logic here
-            ClientCaffe client = new ClientCaffe();
-            
+            ClientCaffe cl = new ClientCaffe();
+
+            String risposta_lista;
+            risposta_lista = cl.richiedi_lista();
+
+            String[] lista_codici = risposta_lista.split(",");
+            String[] lista_caffe = null;
+
+            Scanner input = new Scanner(System.in);
+
+            int scelta = 0;
+            String domanda;
+            String risposta;
+
+            int scelta_caffe;
+            int conta = 0;
+
+            for (String caffe : lista_codici) {
+
+                lista_caffe[conta] = "Caffe " + conta + 1;
+                conta++;
+
+            }
+
+            do {
+
+                System.out.println("Menu:\n\n1 Vota caffe'\n2 Visualizza voto medio\n3 Esci\nScelta: ");
+                scelta = input.nextInt();
+
+                if (scelta == 1) {
+
+                    System.out.println("Quale caffe' vorresti votare?: ");
+                    System.out.println(lista_caffe);
+
+                    scelta_caffe = input.nextInt();
+
+                    try {
+                        int voto;
+                        System.out.println("Inserisci il voto di " + lista_caffe[scelta_caffe - 1] + ", codice: " + lista_codici[scelta_caffe - 1] + ": ");
+
+                        voto = input.nextInt();
+                        domanda = "VOTA," + lista_codici[scelta_caffe - 1] + "," + voto;
+                        cl.output.write(domanda);
+
+                    } catch (IOException ex) {
+                        Logger.getLogger(ClientCaffe.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+                if (scelta == 2) {
+
+                    System.out.println("Di quale caffe' vorresti vedere la media dei voti?: ");
+                    System.out.println(lista_caffe);
+
+                    scelta_caffe = input.nextInt();
+
+                    try {
+
+                        domanda = "MEDIA," + lista_codici[scelta_caffe - 1];
+                        cl.output.write(domanda);
+
+                        risposta = null;
+
+                        while (risposta == null) {
+
+                            risposta = input.toString();
+
+                        }
+
+                        System.out.println("La media di " + lista_caffe[scelta_caffe - 1] + " è: " + risposta);
+
+                    } catch (IOException ex) {
+                        Logger.getLogger(ClientCaffe.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+
+            } while (scelta != 3);
+
         } catch (IOException ex) {
             System.out.println("Errore creazione client");
         }
-        
-        String risposta_lista;
-        risposta_lista = richiedi_lista();
-        
-        String[] lista_codici = risposta_lista.split(",");
-        String[] lista_caffe = null;
-        
-        Scanner input = new Scanner(System.in); 
-        int scelta = 0;
-        String risposta;
-        
-        do{
-        
-            System.out.println("Menu:\n\n1 Vota caffe'\n2 Visualizza voto medio\n3 Esci\nScelta: ");
-            scelta = input.nextInt();
-            
-            if(scelta == 1){
-                
-                int  scelta_voto;
-                
-                System.out.println("Quale caffe' vorresti votare?");
-                
-                int conta = 0;
-                
-                for(String caffe : lista_codici){
-                
-                    lista_caffe[conta] = "Caffe" + conta +1 ;
-                    conta++;
-                
-                }
-                
-                try {
-                    int voto;
-                    System.out.println("Inserisci il voto: ");
-                    
-                    voto = input.nextInt();
-                    risposta = "voto," + voto;
-                    output.write(risposta);
-                    
-                    
-                } catch (IOException ex) {
-                    Logger.getLogger(ClientCaffe.class.getName()).log(Level.SEVERE, null, ex);
-                } 
-            }
-            
-            if(scelta == 2){
-                
-                try {
-                    int voto;
-                    System.out.println("Inserisci il voto: ");
-                    
-                    voto = input.nextInt();
-                    risposta = "voto," + voto;
-                    output.write(risposta);
-                    
-                    
-                } catch (IOException ex) {
-                    Logger.getLogger(ClientCaffe.class.getName()).log(Level.SEVERE, null, ex);
-                } 
-            }
-            
-        }while(scelta!=3);
 
     }
-    
-    public String richiedi_lista(){
-    
+
+    public String richiedi_lista() {
+
         try {
-            output.write("lista");
+            output.write("LISTA");
         } catch (IOException ex) {
             Logger.getLogger(ClientCaffe.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         String risposta = null;
-        
-        while(risposta == null){
+
+        while (risposta == null) {
             risposta = input.toString();
         }
-        
+
         return risposta;
-    
+
     }
 
 }
